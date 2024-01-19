83d3524f452031e32613f4fb4262ef4a30ee619e
Miyu1008 <yamori304@gmail.com>
2024-01-19 20:50:35 +0900

    kadai_007 tuika

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,22 +1,20 @@
 // 任意の数値を変数numに代入する（ここでは10とする）
-const num = 10;
+let num = 15;
 
+// 変数numが3の倍数かつ5の倍数であるか判定する
+if (num % 3 === 0 && num % 5 === 0) {
+  console.log("3と5の倍数です");
+}
 
 // 変数numが3の倍数であるか判定する
-if (num % 3 === 0) {
+else if (num % 3 === 0) {
   console.log("3の倍数です");
 }
 
 // 変数numが5の倍数であるか判定する
-if (num % 5 === 0) {
+else if (num % 5 === 0) {
   console.log("5の倍数です");
 }
-
-// 変数numが3の倍数かつ5の倍数であるか判定する
-if (num % 3 === 0 && num % 5 === 0) {
-    console.log("3と5の倍数です");
-  }
-
 else{
     console.log(num);
 }
